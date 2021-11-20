df7ad8f901e797eb0ccc15a45176fad04ade07e6
Maxon <orumaxon@ya.ru>
2021-11-20 15:10:52 +0400

    переименование названия проекта, добавление requirements

--- partycheck/partycheck/asgi.py
+++ partycheck/partycheck/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for pch project.
+ASGI config for partycheck project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pch.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'partycheck.settings')
 
 application = get_asgi_application()
--- partycheck/partycheck/urls.py
+++ partycheck/partycheck/urls.py
@@ -1,4 +1,4 @@
-"""pch URL Configuration
+"""partycheck URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
