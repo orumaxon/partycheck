3e2edfd8a8f21fc4a4ef052aab35a1dbdf6f0233
Maxon <orumaxon@ya.ru>
2022-02-12 14:55:00 +0400

    dropdown dark menu

--- partycheck/core/models.py
+++ partycheck/core/models.py
@@ -16,10 +16,8 @@ class PartyGroup(CreatedAtMixin, models.Model):
         verbose_name_plural = 'Компании/группы'
 
     name = models.CharField(verbose_name='Наименование', max_length=150)
-    creator = models.ForeignKey(User, models.SET_NULL, verbose_name='Создатель',
-                                null=True, related_name='party_groups')
-    members = models.ManyToManyField(User, verbose_name='Участники',
-                                     blank=True, related_name='party_members')
+    creator = models.ForeignKey(User, models.SET_NULL, verbose_name='Создатель', null=True, related_name='party_groups')
+    members = models.ManyToManyField(User, verbose_name='Участники', blank=True, related_name='party_members')
 
     def __str__(self):
         return f'#{self.id} Компания: {self.name} ({self.members.count()})'
@@ -67,11 +65,11 @@ class PartyPayment(CreatedAtMixin, models.Model):
     party_group = models.ForeignKey(PartyGroup, models.CASCADE, verbose_name='Компания/группа',
                                     related_name='party_payments')
     created_at = models.DateTimeField(verbose_name='Дата создания', auto_now_add=True)
-    investor = models.ForeignKey(User, models.SET_NULL, verbose_name='Кто платил',
-                                 null=True, related_name='party_payments')
+    investor = models.ForeignKey(User, models.SET_NULL, verbose_name='Кто платил', null=True,
+                                 related_name='party_payments')
     price = models.FloatField(verbose_name='Сумма расходов')
-    debtors = models.ManyToManyField(User, verbose_name='На кого делить расходы',
-                                     blank=True, related_name='party_debtors')
+    debtors = models.ManyToManyField(User, verbose_name='На кого делить расходы', blank=True,
+                                     related_name='party_debtors')
     comment = models.CharField(verbose_name='Краткий комментарий', max_length=300, blank=True, null=True)
 
     def __str__(self):
